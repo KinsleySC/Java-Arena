cce5c2620e1f5c8c2193a4ab8f89cbffa4775485
Kinsley SUEDILE COLLET <kinsley.suedile-collet@epitech.eu>
2026-01-26 12:24:34 +0100

    [ADD] I initialized Bestiaire.java with system random monster.

--- Bestiaire.java
+++ Bestiaire.java
@@ -1,10 +1,36 @@
-import java.lang.annotation.ElementType;
-
+import java.util.Random;
 public class Bestiaire {
-    public Monstre genererMonstreAleatoire() {
-        return new Monstre("Monstre Test", 100, 10, Element.FEU);
-    }
-    public Monstre genererMonstreSauvage() {
-        return new Monstre("Sauvage", 150, 15, Element.EAU);
-    }
+   private Random random = new Random();
+   public Bestiaire() {
+   }
+
+   public Monstre creerMonstrePlante() {
+      return new Monstre("Herbizard", 100, 15, ElementType.PLANTE);
+   }
+
+   public Monstre creerMonstreEau() {
+      return new Monstre("Aquatique", 120, 12, ElementType.EAU);
+   }
+
+   public Monstre creerMonstreFeu() {
+      return new Monstre("Flammeche", 110, 14, ElementType.FEU);
+   }
+
+   public Monstre genererMonstreAleatoire() {
+      int var1 = this.random.nextInt(3);
+      if (var1 == 0) {
+         return this.creerMonstrePlante();
+      } else {
+         return var1 == 1 ? this.creerMonstreEau() : this.creerMonstreFeu();
+      }
+   }
+
+   public Monstre genererMonstreSauvage() {
+      int var1 = this.random.nextInt(3);
+      if (var1 == 0) {
+         return new Monstre("Aquatique Sauvage", 200, 12, ElementType.EAU);
+      } else {
+         return var1 == 1 ? new Monstre("Flammeche Sauvage", 200, 14, ElementType.FEU) : new Monstre("Herbizard Sauvage", 200, 15, ElementType.PLANTE);
+      }
+   }
 }
